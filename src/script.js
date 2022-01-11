7086d9e6c80c44bd6e71333bb9a5431870c21d91
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2022-01-11 23:21:48 +0200

    position scale group

--- src/script.js
+++ src/script.js
@@ -8,12 +8,39 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Object
+ * Axes Helper
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 3;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
 
 /**
  * Sizes
@@ -27,10 +54,12 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 4;
 camera.position.x = 1;
 camera.position.y = 1;
+// camera.lookAt(new THREE.Vector3(0, - 1, 0))
 scene.add(camera);
+camera.lookAt(group.position);
 
 /**
  * Renderer
