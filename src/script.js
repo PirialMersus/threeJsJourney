7f7340624eea9ce4a9b81fe4c9e19afcf8312e8d
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2022-01-12 23:53:43 +0200

    animation added

--- src/script.js
+++ src/script.js
@@ -1,71 +1,43 @@
 import "./style.css";
 import * as THREE from "three";
-
+import gsap from "gsap";
+console.log(gsap);
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Axes Helper
- */
-const axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-/**
- * Objects
- */
-const group = new THREE.Group();
-group.scale.y = 2;
-group.rotation.y = 3;
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube1.position.x = -1.5;
-group.add(cube1);
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube2.position.x = 0;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube3.position.x = 1.5;
-group.add(cube3);
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
   width: 800,
   height: 600,
 };
-
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 4;
-camera.position.x = 1;
-camera.position.y = 1;
-// camera.lookAt(new THREE.Vector3(0, - 1, 0))
+camera.position.z = 3;
 scene.add(camera);
-camera.lookAt(group.position);
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+
+const tick = () => {
+  //   mesh.position.x = Math.sin(elapsedTime);
+  //   mesh.position.y = Math.cos(elapsedTime);
+  //   mesh.position.z = Math.cos(elapsedTime);
+  //   camera.lookAt(mesh.position);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
