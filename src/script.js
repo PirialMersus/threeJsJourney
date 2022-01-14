3fe3ec5e14bcd2820f690a5ba4fcf315aa3b756a
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2022-01-14 23:49:01 +0200

    camera aspects

--- src/script.js
+++ src/script.js
@@ -1,43 +1,57 @@
-import "./style.css";
-import * as THREE from "three";
-import gsap from "gsap";
-console.log(gsap);
+import './style.css'
+import * as THREE from 'three'
+
+/**
+ * Base
+ */
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// Scene
-const scene = new THREE.Scene();
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
-};
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
-gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
 
-const tick = () => {
-  //   mesh.position.x = Math.sin(elapsedTime);
-  //   mesh.position.y = Math.cos(elapsedTime);
-  //   mesh.position.z = Math.cos(elapsedTime);
-  //   camera.lookAt(mesh.position);
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
-};
-tick();
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    mesh.rotation.y = elapsedTime;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
